43068e05999de16a8e98e29022034f5c51d37ba7
CableBK <dev.witchapat@gmail.com>
2024-11-08 00:24:17 +0700

    พิธีไหว้ครู 20241108

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
+/*พิธีไหว้ครู 20241108*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! 2024 ไหว้ครูจาวา");
     }
 }
